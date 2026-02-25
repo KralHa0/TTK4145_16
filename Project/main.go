8589d237eeddb08790f175a38d40d643ba310782
daniel <danievh@stud.ntnu.no>
2026-02-25 10:29:49 +0100

    skrevet hele statemachinen pluss timers

--- Project/main.go
+++ Project/main.go
@@ -1,5 +1,25 @@
 package main
 
+
+
+
+import (
+	//def "github.com/KralHa0/TTK4145_16/Project/Definitions"
+	def "Project/Definitions"
+	"Project/Definitions/StateMachine"   //cannot build, because cannot import.
+
+)
+
+
+
+
+
+
 func main() {
+
+
+
+	state_machine.init_state_machine() 
+
 	
 }

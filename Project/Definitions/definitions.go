4e6ccc1d404d3118f86ab5572d4dfc0d2dc84f33
Sanntidssal plass 18 <sanntid@finnesikke.no>
2026-02-25 11:24:57 +0100

    Refractor definitions

--- Project/Definitions/definitions.go
+++ Project/Definitions/definitions.go
@@ -1,49 +1,63 @@
-//Definitions, constants, which are used by all modules.
-
 package definitions
 
 import (
-	"Project/Hardware/elevio/elevator_io"
-	"os"
+	"Driver-go/elevio"
+	"time"
 )
-
-// Default values, elevator:
-var Addr = "localhost"
-var Port = 15657
-
-const num_floors = 4
-const num_buttons = 3     //the 3 are: 0:hallup, 1:halldown, 2:cab. Defined in elevio module.
-const between_floors = -1 //a floor-value used under init.
-const ground_floor = 0
-
-// consts for timers
-const door_open_timeout = 3000 // ms. (door should be open for 3 seconds, then close).
-const watchdog_timeout = 5000  // ms  (should not use more than 5 seconds to move between 2 floors)
-
-// states for instantiated Elevator:
-type possible_states int
 
 const (
-	moving possible_states = iota
-	idle
-	door_open
+	NumFloors     = 4
+	NumButtons    = 3
+	BetweenFloors = -1 //a floor-value used under init.
+	GroundFloor   = 0
+	MsgFrq        = 100 * time.Millisecond
+	Addr          = "localhost"
+	Port          = 15657
+
+	DoorOpenTimeout = 3000 * time.Millisecond // door should be open for 3 seconds, then close
+	WatchdogTimeout = 5000 * time.Millisecond
 )
 
-// struct to hold elevator values
-type Elevator struct {
-	state         possible_states
-	dir     	  elevio.MotorDirection
-	current_floor int
-	ID            int
-	is_obstructed bool //default value = false
+type PossibleStates int
+
+const (
+	Moving PossibleStates = iota
+	Idle
+	DoorOpen
+)
+
+type OrderMessage struct {
+	Floor     int
+	Direction elevio.MotorDirection
 }
 
-// struct so the the modules can message-pass about:
-//
-//	"where is the elevator currently",
-//	"which order has been cleared?",
-//	"which floor should I go to next" etc.
-type floor_and_dir struct {
-	floor     int
-	dir elevio.MotorDirection
+type OrderState uint8
+
+const (
+	NoCall       OrderState = 0
+	Exist        OrderState = 1
+	Acknowledged OrderState = 2
+	Complete     OrderState = 3
+)
+
+type Elevator struct {
+	CurrentFloor  int
+	Direction     elevio.MotorDirection
+	ElevState     PossibleStates
+	Malfunctioned bool
+}
+
+type Node struct {
+	ID          string
+	CabRequests [NumFloors]OrderState
+	Elevator    Elevator
+}
+
+type Worldview struct {
+	Nodes        []Node
+	HallRequests [NumFloors][2]OrderState
+}
+
+type AliveList struct {
+	Peers map[string]bool //Map of peer IDs to their alive status
 }
